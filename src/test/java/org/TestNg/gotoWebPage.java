a8ad0cec60c428592fa19b8d0e31907b95b5c16a
Esizsu <seray.79@hotmail.com>
2020-12-15 16:32:32 -0800

    created new Freamwork

---
+++ src/test/java/org/TestNg/gotoWebPage.java
@@ -0,0 +1,15 @@
+package org.TestNg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class gotoWebPage {
+
+        @Test
+        public void gotoWebUniv() {
+            System.setProperty("webdriver.chrome.driver", "webdriver\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("http://www.webdriveruniversity.com/Click-Buttons/index.html");
+        }
+}
